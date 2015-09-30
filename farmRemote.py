1e828d97323e1fc6b3c4aa9c344f11e6cc8345f0
James Bartolozzi <jamesbartolozzi@Jamess-Mac-Pro.local>
2015-09-30 10:34:07 -0400

    added farm remote
    Farm remote tool for sending remote jobs

---
+++ farmRemote.py
@@ -0,0 +1,23 @@
+
+from PySide import QtCore
+from PySide import QtGui
+from PySide import QtWebKit
+
+import os
+import sys
+
+class QuickStartBrowser(QtWebKit.QWebView):
+    def __init__(self):
+        QtWebKit.QWebView.__init__(self)
+        
+    def contextMenuEvent(self, event):
+        menu = QtGui.QMenu()
+        menu.addAction(self.pageAction(QtWebKit.QWebPage.Copy))
+        menu.exec_(QtGui.QCursor.pos())
+
+def createInterface():
+
+    web_view = QuickStartBrowser()
+    web_view.load(QtWebKit.QUrl('http://25.125.36.248:5000'))
+
+    return web_view
